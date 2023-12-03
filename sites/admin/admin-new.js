8f866305f5043eb77622a97282c361de12cf711d
display <144018979+nhuijser@users.noreply.github.com>
2023-12-03 12:03:03 +0100

    feat: final changes
    taking a break

--- sites/admin/admin-new.js
+++ sites/admin/admin-new.js
@@ -2,19 +2,29 @@ function openProject(id) {
     const select = document.getElementById(id);
  
     if(select.getAttribute("selected") == "false")  {
-        select.setAttribute("selected", "true");
-        const p = document.createElement('p');
-        p.innerHTML = select.getAttribute("description");
-        select.appendChild(p);
-        select.style.height = "150vh;";
         select.style.transition = "0.5s";
+        select.setAttribute("selected", "true");;
+        const p = document.createElement('p');
+        p.innerHTML = select.getAttribute("description");;
+        select.appendChild(p);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = "Delete";
+        deleteButton.setAttribute("onclick", "deleteProject('" + id + "')");
+        deleteButton.setAttribute("class", "delete-button");
+        select.appendChild(deleteButton);
         return;
     } else {
-        select.setAttribute("selected", "false");
-        select.style.height = "10vh;";
         select.style.transition = "0.5s";
+        select.setAttribute("selected", "false");
         select.removeChild(select.lastChild);
+        select.removeChild(select.lastChild);
+
         return;
     }
+
+}
+
+function deleteProject(id) {    
 
 }
